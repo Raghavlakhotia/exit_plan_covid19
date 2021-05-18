b9e9881758d7920ac891d27ba830ab2e94758907
deepanshugarg007 <41945918+deepanshugarg007@users.noreply.github.com>
2021-05-18 17:53:45 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -36,7 +36,7 @@ def isPregnant():
 
 
 def generate_bmi():
-    num = random.randint(0,4)
+    num = random.randint(0,5)
     bmi_list = ["Thinness (< 18.5)", "Normal (18.5 - 25)", "Overweight (25 - 30)", "Obese Class I (30 - 35)",
                 "Obese Class II (35 - 40)", "Obese Class III (> 40)"]
     return bmi_list[num];
